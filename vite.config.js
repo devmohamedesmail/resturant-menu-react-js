0e44bdafa54babb6af608cf2878529619f8c157f
Mohamed Esmail <dev.mohamed.esmail@gmail.com>
2025-02-01 21:40:15 +0400

    update

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  build: {
+    outDir: 'dist', // Ensure the build output directory is correctly set to 'dist'
+  },
 })
