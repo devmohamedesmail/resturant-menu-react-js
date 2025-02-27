cc7574451dbbd5b01e6b34b1e7121aef1fbb0217
Mohamed Esmail <dev.mohamed.esmail@gmail.com>
2025-02-27 13:43:39 +0400

    Create Bottom bar

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Banner() {
   return (
     <div className='my-5  container m-auto px-2 '>
-        <img src="/public/images/banner.jpg" alt="banner" className='w-full h-full rounded-2xl m-auto' />
+        <img src="/images/banner.jpg" alt="banner" className='w-full h-full rounded-2xl m-auto' />
     </div>
   )
 }
